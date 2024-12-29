b6ed5b7e130b360d0d4900e5ba823876de506f14
Ashutoshnext <ashutoshpal911097@gmail.com>
2024-12-29 17:44:33 +0530

    prob7

---
+++ prob7.js
@@ -0,0 +1,12 @@
+const add=(a,b)=>{
+    return a+b;
+}
+const multiply=(a,b)=>{
+    return a*b;
+}
+
+let addans=add(2,3);
+console.log('add :',addans)
+
+const multiplyans=multiply(3,4);
+console.log('multiply :',multiplyans)
